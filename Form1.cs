c0ec3f284b95575f015458b8e2eea6bea2d16a34
Gordon McCabe <gordon.mccabe921@gmail.com>
2019-03-21 08:47:13 +0800

    Incorrect Functions were corrected and exception handinlg added

--- Form1.cs
+++ Form1.cs
@@ -19,26 +19,91 @@ namespace Activities
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(txtNumber1.Text) || String.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("You need to enter a value in both number boxes for this to work");
+                return;
+            }
+            int parsedValue1;
+            int parsedValue2;
+
+            if (!int.TryParse(txtNumber1.Text, out parsedValue1) || !int.TryParse(txtNumber2.Text, out parsedValue2))
+            {
+                MessageBox.Show("Both numbers must be integers ");
+                return;
+            }
             txtAnswer.Text =  calculateAnswer("Add");
         }
 
         private void btnMultiply_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(txtNumber1.Text) || String.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("You need to enter a value in both number boxes for this to work");
+                return;
+            }
+            int parsedValue1;
+            int parsedValue2;
+
+            if (!int.TryParse(txtNumber1.Text, out parsedValue1) || !int.TryParse(txtNumber2.Text, out parsedValue2))
+            {
+                MessageBox.Show("Both numbers must be integers ");
+                return;
+            }
             txtAnswer.Text = calculateAnswer("Multiply");
         }
 
         private void btnModulus_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(txtNumber1.Text) || String.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("You need to enter a value in both number boxes for this to work");
+                return;
+            }
+            int parsedValue1;
+            int parsedValue2;
+
+            if (!int.TryParse(txtNumber1.Text, out parsedValue1) || !int.TryParse(txtNumber2.Text, out parsedValue2))
+            {
+                MessageBox.Show("Both numbers must be integers ");
+                return;
+            }
             txtAnswer.Text = calculateAnswer("Modulus");
         }
 
         private void btnSubtract_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(txtNumber1.Text) || String.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("You need to enter a value in both number boxes for this to work");
+                return;
+            }
+            int parsedValue1;
+            int parsedValue2;
+
+            if (!int.TryParse(txtNumber1.Text, out parsedValue1) || !int.TryParse(txtNumber2.Text, out parsedValue2))
+            {
+                MessageBox.Show("Both numbers must be integers ");
+                return;
+            }
             txtAnswer.Text = calculateAnswer("Subtract");
         }
 
         private void btnDivide_Click(object sender, EventArgs e)
         {
+            if (String.IsNullOrEmpty(txtNumber1.Text) || String.IsNullOrEmpty(txtNumber2.Text))
+            {
+                MessageBox.Show("You need to enter a value in both number boxes for this to work");
+                return;
+            }
+            int parsedValue1;
+            int parsedValue2;
+
+            if (!int.TryParse(txtNumber1.Text, out parsedValue1) || !int.TryParse(txtNumber2.Text, out parsedValue2))
+            {
+                MessageBox.Show("Both numbers must be integers ");
+                return;
+            }
             txtAnswer.Text = calculateAnswer("Divide");
         }
 
@@ -52,22 +117,22 @@ namespace Activities
             {
                 case "Add":
                     {
-                        sum = number1 - number2;
+                        sum = number1 + number2;
                         break;
                     }
                 case "Subtract":
                     {
-                        sum = number1 + number2;
+                        sum = number1 - number2;
                         break;
                     }
                 case "Multiply":
                     {
-                        sum = number1 / number2;
+                        sum = number1 * number2;
                         break;
                     }
                 case "Divide":
                     {
-                        sum = number1 * number2;
+                        sum = number1 / number2;
                         break;
                     }
                 case "Modulus":
